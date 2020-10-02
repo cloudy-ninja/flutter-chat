7ef5d168184cdbaa3d74997cddea897e4fc98a02
cloudy-ninja <msr0327@outlook.com>
2020-10-02 15:51:17 +0800

    Set up the routes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
     );
   }
 }
